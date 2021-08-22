7e1b2a81b15872a8c85b566682efca91ea93607f
anirban das <anirbanstifler@gmail.com>
2021-08-22 20:02:24 +0530

    added shell script for operations purpose

---
+++ mvnw.sh
@@ -0,0 +1,2 @@
+mvn clean package -e
+# mvn install:install-file -DgroupId=com.teenthofabud.core.common -DartifactId=toab-chassis-sdk -Dpackaging=jar -Dversion=0.0.1-SNAPSHOT -Dfile=target\toab-chassis-sdk-0.0.1-SNAPSHOT.jar -DgeneratePom=true -e
