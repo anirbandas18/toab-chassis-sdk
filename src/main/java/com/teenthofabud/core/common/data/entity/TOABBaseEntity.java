7e1498d2234a9bc577accd61e7ea18ed6b127e97
anirbandas18 <Razor1911$january>
2021-11-08 03:59:07 +0530

    added auto audit support for JPA models

--- src/main/java/com/teenthofabud/core/common/data/entity/TOABBaseEntity.java
+++ src/main/java/com/teenthofabud/core/common/data/entity/TOABBaseEntity.java
@@ -4,6 +4,10 @@ package com.teenthofabud.core.common.data.entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
@@ -18,15 +22,19 @@ public abstract class TOABBaseEntity {
 
     @Column(name = "created_on")
     @ToString.Include
+    @CreatedDate
     private LocalDateTime createdOn;
     @Column(name = "modified_on")
     @ToString.Include
+    @LastModifiedDate
     private LocalDateTime modifiedOn;
     @Column(name = "created_by")
     @ToString.Include
+    @CreatedBy
     private Long createdBy;
     @Column(name = "modified_by")
     @ToString.Include
+    @LastModifiedBy
     private Long modifiedBy;
     @Column(name = "active_sw")
     @ToString.Include
