2ae922432f5deba62d43679458dcf84492bbbc92
anirbandas18 <Razor1911$january>
2021-11-08 00:55:57 +0530

    updated version, removed bean creation from unrelated auto configuration

--- src/main/java/com/teenthofabud/core/common/configuration/TOABAutoConfiguration.java
+++ src/main/java/com/teenthofabud/core/common/configuration/TOABAutoConfiguration.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.teenthofabud.core.common.factory.TOABRepositoryDynamicQueryBuilderFactory;
 import com.teenthofabud.core.common.handler.TOABBaseAuditPropertyHandler;
 import com.teenthofabud.core.common.handler.TOABMessageSource;
 import com.teenthofabud.core.common.service.TOABBaseService;
@@ -54,11 +53,6 @@ public class TOABAutoConfiguration {
     @Bean
     public TOABBaseService toabBaseService() {
         return new TOABBaseServiceImpl();
-    }
-
-    @Bean
-    public TOABRepositoryDynamicQueryBuilderFactory dynamicQueryBuilderFactory() {
-            return new TOABRepositoryDynamicQueryBuilderFactory();
     }
 
     @Bean
