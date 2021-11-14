cdae8de8d2af879bab11ebcd376708d446af9764
anirbandas18 <Razor1911$january>
2021-11-14 14:21:42 +0530

    added context support for validator flows that require additional parameters

---
+++ src/main/java/com/teenthofabud/core/common/converter/TOABBaseEntity2VoConverter.java
@@ -0,0 +1,27 @@
+package com.teenthofabud.core.common.converter;
+
+import com.teenthofabud.core.common.constant.TOABCascadeLevel;
+import com.teenthofabud.core.common.data.dto.TOABRequestContextHolder;
+import com.teenthofabud.core.common.data.entity.TOABBaseEntity;
+import com.teenthofabud.core.common.data.vo.TOABBaseVo;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class TOABBaseEntity2VoConverter<S extends TOABBaseEntity,T extends TOABBaseVo> {
+
+    public void expandAuditFields(S entity, T vo) {
+        TOABCascadeLevel cascadeLevel = TOABRequestContextHolder.getCascadeLevelContext();
+        switch (cascadeLevel) {
+            case ONE:
+                vo.setActive(entity.getActive());
+                vo.setModifiedOn(entity.getModifiedOn());
+                vo.setCreatedOn(entity.getCreatedOn());
+                vo.setModifiedBy(entity.getModifiedBy());
+                vo.setCreatedBy(entity.getCreatedBy());
+                break;
+            case TWO:
+                log.debug("No eligible fields available in {} to perform two level cascading in target type");
+        }
+    }
+
+}
---
+++ src/main/java/com/teenthofabud/core/common/data/dto/TOABValidationContextHolder.java
@@ -0,0 +1,33 @@
+package com.teenthofabud.core.common.data.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class TOABValidationContextHolder {
+
+    private static final ThreadLocal<Map<String,Object>> SUPPORTING_VALIDATION_PARAMETER_CONTEXT = ThreadLocal.withInitial( () -> {
+        return new HashMap<>();
+    });
+
+    public static void setSupportingValidationParameterContext(String key, String value) {
+        Map<String,Object> parameters = SUPPORTING_VALIDATION_PARAMETER_CONTEXT.get();
+        parameters.put(key, value);
+        SUPPORTING_VALIDATION_PARAMETER_CONTEXT.set(parameters);
+    }
+
+    public static Optional<Object> getSupportingValidationParameterContext(String key) {
+        Map<String,Object> parameters = SUPPORTING_VALIDATION_PARAMETER_CONTEXT.get();
+        if(parameters.isEmpty()) {
+            return Optional.empty();
+        } else {
+            Optional<Object> value = Optional.of(parameters.get(key));
+            return value;
+        }
+    }
+
+    public static void clearSupportingValidationParameterContext() {
+        SUPPORTING_VALIDATION_PARAMETER_CONTEXT.remove();
+    }
+
+}
