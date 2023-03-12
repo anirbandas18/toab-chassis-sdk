3829253950f020097933d6500b6352383c3bda9c
adas <anirban.das@ionos.com>
2023-03-12 20:11:05 +0100

    release version 1.8.5-SNAPSHOT with support for unproxying hibernate entities to better enable entity 2 vo conversion of child entities from parent entity 2 vo converter

--- src/main/java/com/teenthofabud/core/common/converter/TOABBaseEntity2VoConverter.java
+++ src/main/java/com/teenthofabud/core/common/converter/TOABBaseEntity2VoConverter.java
@@ -4,6 +4,8 @@ import com.teenthofabud.core.common.constant.TOABCascadeLevel;
 import com.teenthofabud.core.common.data.dto.TOABRequestContextHolder;
 import com.teenthofabud.core.common.data.entity.TOABBaseEntity;
 import com.teenthofabud.core.common.data.vo.TOABBaseVo;
+import com.teenthofabud.core.common.error.TOABErrorCode;
+import com.teenthofabud.core.common.error.TOABSystemException;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.temporal.ChronoUnit;
@@ -27,6 +29,14 @@ public abstract class TOABBaseEntity2VoConverter<S extends TOABBaseEntity,T exte
             default:
                 log.debug("No eligible fields available in {} to perform two level cascading in target type");
                 break;
+        }
+    }
+
+    public <T> T renderInstanceOfType(Object o, Class<T> clazz) {
+        try {
+            return clazz.cast(o);
+        } catch(ClassCastException e) {
+            throw new TOABSystemException(TOABErrorCode.SYSTEM_INTERNAL_ERROR, "Could not unproxy from " + o.getClass() + " to " + clazz);
         }
     }
 
